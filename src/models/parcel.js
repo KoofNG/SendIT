ec3d1812aeb817eec86e9a49bdf99a947d7a0e78
KoofNG <isrealakpan296@gmail.com>
2019-05-05 04:16:42 +0100

    Created database model for User and Parcel

---
+++ src/models/parcel.js
@@ -0,0 +1,42 @@
+const parcel = {
+    id: {
+        type: Number
+    },
+    placedBy: {
+        type: Number
+    },
+    weight: {
+        type: Number
+    },
+    weightMetric: {
+        type: String
+    },
+    placedOn: {
+        type: Date
+    },
+    sentOn: {
+        type: Date
+    },
+    delieverdOn: {
+        type: Date
+    },
+    Status: {
+        type: String
+    },
+    from: {
+        type: String
+    },
+    to: {
+        type: String
+    },
+    currentLocation: {
+        type: String
+    },
+    cancelled: {
+        type: Boolean,
+        default: false
+    }
+
+}
+
+export default parcel;
---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+const user = {
+  id: { type: Number },
+  firstname: { type: String },
+  lastname: { type: String },
+  otherName: { type: String },
+  email: { type: String },
+  username: { type: String },
+  password: { type: String },
+  registered: { type: Date },
+  isAdmin: { type: Boolean }
+};
+
+
+export default user;
