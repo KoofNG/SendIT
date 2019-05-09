eed7fdbaa7233525c0a6d5c5de58a472fb239b01
KoofNG <isrealakpan296@gmail.com>
2019-05-09 23:18:20 +0100

    Deleted the server file

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import { http } from 'http';
-import { dotEnv } from 'dotenv';
-
-const env = dotEnv.config();
-const server = http.createServer((req,res) => {
-    res.end('Server created!');
-})
-
-server.listen(process.env.PORT);
