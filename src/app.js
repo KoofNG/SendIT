f2125f64de271a7cd0a6090541a152ccdd36324b
KoofNG <isrealakpan296@gmail.com>
2019-05-09 23:19:23 +0100

    Added Postgres Client Interface

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+// Importing the various file dependencies for this file
 import express from 'express';
+import dotEnv from 'dotEnv';
+
+
+// Connection to Database
+import pg from "pg";
+
+dotEnv.config();
+
+// Importing the various routes for each case scenarios;
+import parcels from './routes/parcel';
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use()
+app.use('/parcels', parcels);
+
+app.listen(process.env.PORT, () => console.log(`Server listening on ${process.env.PORT}`));
