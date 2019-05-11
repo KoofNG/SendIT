b31147cb28483eab157003fe06bf90c0931f490e
KoofNG <isrealakpan296@gmail.com>
2019-05-11 10:12:21 +0100

    Added more dependencies for the application

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 // Importing the various file dependencies for this file
 import express from 'express';
 import dotEnv from 'dotEnv';
+import bodyParser from "body-parser";
+import cors from 'cors';
+import jsonwebtoken from 'jsonwebtoken';
+import jwt from 'express-jwt';
+
 
 
 // Connection to Database
