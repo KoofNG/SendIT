5073fe33d43116e91fcb6f137b449196a58e6331
KoofNG <isrealakpan296@gmail.com>
2019-05-13 11:05:36 +0100

    Implemented the Sign up endpoint
    It returns the full JSON data as required in the specification

--- src/controllers/resources/dbQueries.js
+++ src/controllers/resources/dbQueries.js
@@ -8,69 +8,70 @@ import client from "./client";
 import user from "../../models/user";
 import Payload from "../../models/payload";
 
+const dbQueries = {
 
-// For Sign up
-function doSignUp() {
-    
-}
+    // For Sign up
+    doSignUp(req, res) {
+        const newUser = new user();
+        const salt = bcrypt.genSaltSync();
+        const hashedPassword = bcrypt.hashSync(req.body.password.trim(), salt);
 
-// For Login
+        var savedUser;
 
+        if (
+            req.body.firstname.length != 0 &&
+            req.body.lastname.length != 0 &&
+            req.body.email.length != 0 &&
+            req.body.password.length != 0
+        ) {
+            newUser.firstname = req.body.firstname.toLowerCase().trim();
+            newUser.lastname = req.body.lastname.toLowerCase().trim();
+            newUser.othername = req.body.othername.toLowerCase().trim();
+            newUser.email = req.body.email.toLowerCase().trim();
+            newUser.username = req.body.username.toLowerCase().trim();
+            newUser.password = hashedPassword;
+            newUser.registered = new Date().toUTCString();
+            newUser.isAdmin = false;
 
-authorize.post("/signup", (req, res) => {
-    const newUser = new user();
-    const salt = bcrypt.genSaltSync();
-    const hashedPassword = bcrypt.hashSync(req.body.password.trim(), salt);
-    var token;
-    var savedUser;
+            const text = "INSERT INTO users(firstname,lastname,othername,email,username,password,registered,isAdmin) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *";
+            const values = [`${newUser.firstname}`,`${newUser.lastname}`,`${newUser.othername}`,`${newUser.email}`,`${newUser.username}`,`${newUser.password}`,`${newUser.registered}`,`${newUser.isAdmin}`];
 
-    if (req.body.firstname.length != 0  && req.body.lastname.length != 0 && req.body.email.length != 0 && req.body.password.length != 0){
-        newUser.firstname = req.body.firstname.toLowerCase().trim();
-        newUser.lastname = req.body.lastname.toLowerCase().trim();
-        newUser.othername = req.body.othername.toLowerCase().trim();
-        newUser.email = req.body.email.toLowerCase().trim();
-        newUser.username = req.body.username.toLowerCase().trim();
-        newUser.password = hashedPassword;
-        newUser.registered = new Date().toUTCString();
-        newUser.isAdmin = false;
-
-        const text = 'INSERT INTO users(firstname,lastname,othername,email,username,password,registered,isAdmin) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *'
-        const values = [`${newUser.firstname}`, `${newUser.lastname}`, `${newUser.othername}`, `${newUser.email}`, `${newUser.username}`, `${newUser.password}`, `${newUser.registered}`, `${newUser.isAdmin}`]
-
-
-        client.query(text, values)
-            .then(res => {
-                // console.log('Data inserted successfully');
-                savedUser = res.rows[0];
-                console.log(".......................")
-                console.log(savedUser);
-                console.log(".......................")
-
-
-                const payload = new Payload(savedUser.firstname, savedUser.lastname, savedUser.email, savedUser.username);
-                console.log("****************");
-                console.log(payload);
-                console.log("****************");
-
-                token = jwt.sign(JSON.stringify(payload), "12345");
-                console.log(token);
-            })
-            .catch(e => console.error(e.stack));
-
-        res.json({
-            status: 200,
-            token: token,
-            message: "Data inserted successfully"
-        });
-
-    } else {
-        res.json({
-            status: 401,
-            message: "Fill in required fields"
-        });
+            client
+                .query(text, values)
+                .then(data => {
+                    savedUser = data.rows[0];
+                })
+                .then(() => {
+                    const payload = new Payload(savedUser.firstname,savedUser.lastname,savedUser.email,savedUser.username);
+                    const token = jwt.sign(JSON.stringify(payload), "12345");
+                    res.json({
+                        status: 200,
+                        data: [{
+                            token : token,
+                            user : {
+                                firstname: savedUser.firstname,
+                                lastname: savedUser.lastname,
+                                email: savedUser.email,
+                                username: savedUser.username
+                            }
+                        }],
+                        message: "User created successfully"
+                    });
+                    client.end();
+                })
+                .catch(err => console.error(err));
+        } else {
+            res.json({
+                status: 401,
+                message: "Fill in required fields"
+            });
+        }
     }
-});
 
-authorize.post("/login", (req, res) => { });
+    // For Login
+    // doLogin(req, res, next) {
+    //     const hashedPassword = ();
+    // }
+};
 
-export default authorize;
+export default dbQueries;
