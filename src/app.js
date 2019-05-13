b144013077ce49e70d1586d3539b148a8b6944fd
KoofNG <isrealakpan296@gmail.com>
2019-05-13 08:10:21 +0100

    Changed routes
    - Removed unused dependencies
    - Changed the routes parameters
    - Changed the routes to access the various controller routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import express from 'express';
 import dotEnv from 'dotEnv';
 import bodyParser from "body-parser";
 import cors from 'cors';
-import jsonwebtoken from 'jsonwebtoken';
-import jwt from 'express-jwt';
+
+// import jsonwebtoken from 'jsonwebtoken';
+// import jwt from 'express-jwt';
 
 // Importing the various routes for each case scenarios;
-import parcels from './routes/parcel';
-import authorize from './routes/authorization';
+import parcels from './controllers/routes/parcel';
+import user from './controllers/routes/users';
 
 dotEnv.config();
 
@@ -17,9 +18,11 @@ dotEnv.config();
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/parcels', parcels);
-app.use('/auth', authorize);
+app.use('/api/v1/parcels', parcels);
+app.use('/api/v1/auth', user);
 
+app.use(cors());
 app.listen(process.env.PORT, () => console.log(`Server listening on ${process.env.PORT}`));
