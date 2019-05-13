90133893077fca3cb267fbaf87e207771d5c266c
KoofNG <isrealakpan296@gmail.com>
2019-05-13 08:14:25 +0100

    Payload Model for token generation

---
+++ src/models/payload.js
@@ -0,0 +1,9 @@
+class Payload {
+    constructor(firstname,lastname,email,username) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.username = username;
+    }
+}
+export default Payload;
